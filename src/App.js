6f42b25abe189dfa41301f1727ab11075bbb40d1
Sejong Yoo <ysj7705@gmail.com>
2023-06-25 00:21:50 +0900

    무한 스크롤 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import KakaoMap from "./Component/Map/KakaoMap";
 import ChargerList from "./Component/Charger/ChargerList";
 
 function App() {
-  const [chargerInfo, setChargerInfo] = useState([]);
-  const [searchAddr, setSearchAddr] = useState("");
+	const [chargerInfo, setChargerInfo] = useState([]);
 
-  const endPoint =
-    "https://api.odcloud.kr/api/EvInfoServiceV2/v1/getEvSearchList";
-  const addr = searchAddr;
-  const pageNo = 2;
-  const perPage = 50;
-  const returnType = "JSON";
-  const serviceKey =
-    "0D2g1SK%2BiKevG%2FPcWEJ%2FYpST4B82lAizBMQ%2B%2BZsKNjcFZqeiqHddHMUshTHRgaaaVqhk8PwdVECW2l6%2B2pqBmg%3D%3D";
-  console.log(addr);
-  async function getData() {
-    const response = await fetch(
-      `${endPoint}?page=${pageNo}&perPage=${perPage}&returnType=${returnType}&cond%5Baddr%3A%3ALIKE%5D=${addr}&serviceKey=${serviceKey}`
-    );
-
-    return response;
-  }
-  useEffect(() => {
-    getData()
-      .then((res) => res.json())
-      .then((json) => setChargerInfo(json.data));
-  }, [searchAddr]);
-
-  useEffect(() => {
-    console.log(searchAddr, "string");
-  }, [searchAddr]);
-
-  return (
-    <div>
-      <KakaoMap items={chargerInfo} />
-      <ChargerList items={chargerInfo} input={setSearchAddr} />
-    </div>
-  );
+	return (
+		<div>
+			<KakaoMap items={chargerInfo} />
+			<ChargerList chargerInfo={chargerInfo} setChargerInfo={setChargerInfo} />
+		</div>
+	);
 }
 
 export default App;
